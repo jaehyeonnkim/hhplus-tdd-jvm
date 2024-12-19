f6d0e408f0230e1029374a2184c23fb93692fb94
jaehyeonkim <wogus6805@gmail.com>
2024-12-19 16:42:51 +0900

    포인트 조회 비즈니스로직 / 테스트코드

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -1,45 +1,69 @@
 package io.hhplus.tdd.point;
 
+import io.hhplus.tdd.ErrorResponse;
 import io.hhplus.tdd.database.UserPointTable;
+import io.hhplus.tdd.database.PointHistoryTable;
 import org.apache.coyote.BadRequestException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PointService {
 
-    private final UserPointTable userPointTable;
+    private final UserPointTable userPointTable;        //사용자 포인트 데이터
+    private final PointHistoryTable pointHistoryTable;  // 포인트 이력 데이터
+
     private static final long MAX_POINT = 1_000_000;
 
     // 의존성 주입 방식의 생성자
-    public PointService(UserPointTable userPointTable) {
+    public PointService(UserPointTable userPointTable, PointHistoryTable pointHistoryTable) {
         this.userPointTable = userPointTable;
+        this.pointHistoryTable = pointHistoryTable;
+    }
+
+    //포인트 조회
+    public UserPoint getPoint(long id)  {
+        //id가 유효하지 않을 때 (0이하일 때)
+        if(id <= 0){
+            throw new IllegalArgumentException(
+                    new ErrorResponse("INVALID_ID", "유효하지 않은 ID입니다.").toString());
+        }
+        UserPoint userPoint = userPointTable.selectById(id);
+        //포인트 정보가 없을 때
+        if(userPoint == null){
+            throw new NullPointerException(
+                    new ErrorResponse("NULL_EXCEPTION", "포인트 정보를 찾을 수 없습니다.").toString());
+        }
+        return userPoint;
     }
 
     //포인트 충전
-    public UserPoint chargePoints(long point, long userId) throws Exception {
+    public UserPoint chargePoints(long point, long id) throws Exception {
         //포인트가 최대한도거나 0, 음수일 때
         if (point <= 0 || point > MAX_POINT) {
-            throw new BadRequestException("INVALID_AMOUNT");
+            throw new IllegalArgumentException(
+                    new ErrorResponse("INVALID_AMOUNT", "충전할 포인트가 유효하지 않습니다").toString());
         }
 
         //유저의 현재포인트
-        UserPoint userPoint = userPointTable.selectById(userId);
+        UserPoint userPoint = userPointTable.selectById(id);
 
         //새로운 포인트 잔액 계산
         long newBalance = userPoint.point() + point;
 
         //잔고 금액이 최대한도를 넘었을 때
         if (newBalance > MAX_POINT){
-            throw new BadRequestException("EXCEED_BALANCE");
+            throw new IllegalArgumentException(
+                    new ErrorResponse("EXCEED_BALANCE", "최대 잔고 금액을 초과할 수 없습니다.").toString());
         }
 
         //유저 포인트 업데이트
-        userPointTable.insertOrUpdate(userId, newBalance);
+        userPointTable.insertOrUpdate(id, newBalance);
         return userPoint;
     }
 
-    public UserPoint getBalance(long userId) {
-        return userPointTable.selectById(userId);
+    public UserPoint getBalance(long id) {
+        return userPointTable.selectById(id);
     }
+
 
 }
--- src/test/java/io/hhplus/tdd/point/PointServiceTest.java
+++ src/test/java/io/hhplus/tdd/point/PointServiceTest.java
@@ -23,23 +23,66 @@ public class PointServiceTest {
     @InjectMocks //pointService에 userPointTable을 자동으로 주입
     private PointService pointService;
 
+    /**
+     * 1. 포인트 조회 TEST
+     */
+    //id값이 0이하일 때
+    @Test
+    @DisplayName("id가 0일 때 INVALID_ID 에러 메시지 반환")
+    void 포인트_조회_실패_아이디_제로(){
+        assertThatThrownBy(() -> pointService.getPoint(0))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("INVALID_ID");
+
+    }
+
+    //id값이 0미만일 때
+    @Test
+    @DisplayName("id가 0미만일 때 INVALID_ID 에러 메시지 반환")
+    void 포인트_조회_실패_아이디_음수(){
+        assertThatThrownBy(() -> pointService.getPoint(-500))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("INVALID_ID");
+
+    }
+
+    //포인트 정보가 없을 때
+    @Test
+    @DisplayName("포인트 정보가 없을 때 NULL_EXCEPTION 에러 메시지 반환")
+    void 포인트_조회_실패_포인트_널(){
+        //유저가 존재하지 않는 이유등으로 인해,
+        //반환값이 null일 때
+        given(userPointTable.selectById(999_999_999L))
+                .willReturn(null);
+
+        //when & then
+        assertThatThrownBy(() -> pointService.getPoint(999_999_999L))
+                .isInstanceOf(NullPointerException.class)
+                .hasMessageContaining("NULL_EXCEPTION");
+    }
+
+
+    /**
+     * 2. 포인트 충전 TEST
+     */
     //충전금액이 0이하일때
     @Test
-    @DisplayName("-500원 포인트 충전 시 BadRequestException 반환")
+    @DisplayName("-500원 포인트 충전 시 INVALID_AMOUNT 에러메시지 반환")
     void 포인트_충전_실패_음수() {
         //given
-        UserPoint mockUserPoint = new UserPoint(111L, 100, 100);
-        given(userPointTable.selectById(111L)).willReturn(mockUserPoint); // mockUserPoint 반환
+        //UserPoint mockUserPoint = new UserPoint(111L, 100, 100);
+        given(userPointTable.selectById(111L))
+                .willReturn(new UserPoint(111L,100,100)); // mockUserPoint 반환
 
         //then
         assertThatThrownBy(() -> pointService.chargePoints(-500, 111L))
-                .isInstanceOf(BadRequestException.class)
-                .hasMessage("INVALID_AMOUNT");
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("INVALID_AMOUNT");
 
     }
     //충전금액이 최대한도 초과할때
     @Test
-    @DisplayName("2_000_000원 포인트 충전 시 BadRequestException 반환")
+    @DisplayName("2_000_000원 포인트 충전 시 INVALID_AMOUNT 에러메시지 반환")
     void 포인트_충전_실패_최대한도_초과(){
         //given
         given(pointService.getBalance(111L)).willReturn(
@@ -47,13 +90,13 @@ public class PointServiceTest {
         );
         //then
         assertThatThrownBy(() -> pointService.chargePoints(2_000_000, 111L))
-                .isInstanceOf(BadRequestException.class)
-                .hasMessage("INVALID_AMOUNT");
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("INVALID_AMOUNT");
     }
 
     //충전이후 금액이 최대한도를 넘을 때
     @Test
-    @DisplayName("1_000_000원 포인트 충전 시 잔고초과로 BadRequestException 반환")
+    @DisplayName("1_000_000원 포인트 충전 시 잔고초과로 EXCEED_BALANCE 에러메시지 반환")
     void 포인트_충전_실패_잔고_한도초과() {
         //given
         given(pointService.getBalance(111L)).willReturn(
@@ -62,8 +105,8 @@ public class PointServiceTest {
 
         //then
         assertThatThrownBy(() -> pointService.chargePoints(1_000_000, 111L))
-                .isInstanceOf(BadRequestException.class)
-                .hasMessage("EXCEED_BALANCE");
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("EXCEED_BALANCE");
 
     }
 }
